af60335c26417bc1e5a70960588daffc3d02da84
Remmy Clay <remmy.lauren@gmail.com>
2018-01-29 17:50:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,12 +3,12 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  #"SELECT characters.name, characters.motto FROM characters ORDER BY len(characters.motto) DESC LIMIT 1;"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY length(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(*) FROM characters ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
